a4a64b39dd6afb194eefa09c8c18c1eef0674133
Natália <smachadonatalia@gmail.com>
2025-10-11 18:53:15 -0300

    converting to react app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img id="background_image" src="/images/bg-desktop-light.jpg" alt="" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/elements/BackgroundImage.jsx
@@ -0,0 +1,7 @@
+import DesktopLight from "/images/bg-desktop-light.jpg";
+
+function BackgroundImage() {
+  return <img src={{ DesktopLight }} alt="" />;
+}
+
+export default BackgroundImage;
