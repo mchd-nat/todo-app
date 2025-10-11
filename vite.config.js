016cdb92070ee99bb376f94aac29027c60dfde21
Natália <smachadonatalia@gmail.com>
2025-10-11 19:55:21 -0300

    update homepage and base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mchd-nat.github.io/todo-app/",
+  base: "/todo-app/",
 });
